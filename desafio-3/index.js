fa7e44a1c3dc28cf1d7ed473744de3f30b1603d6
strlla <estrellaportocarrero@gmail.com>
2021-01-20 02:01:41 -0300

    Desafío 3 - JavaScript Asincrónico

--- desafio-3/index.js
+++ desafio-3/index.js
@@ -2,17 +2,15 @@ const getWords = (text, fun, sec = 1000) => {
     const words = text.split(' ');
     let countWords = words.length;
     let i = 0;
-    
-        words.forEach((w) => {
-            const interval = setInterval(()=>{
-                fun(w)
-                if( i >= countWords ){
-                    clearInterval(interval);
-                }
-                i++;  
-            }, sec)    
-  
-        })
+    words.forEach((w) => {
+        const interval = setInterval(()=>{
+            fun(w)
+            if (i > countWords){
+                clearInterval(interval);
+            }
+        i++;  
+        }, sec)      
+    })
 }
 
 const show = (w) => console.log(w);
