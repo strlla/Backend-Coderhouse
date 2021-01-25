22d82edd0e4cc3f703e42ced41f871e5fdba8366
strlla <estrellaportocarrero@gmail.com>
2021-01-24 21:14:56 -0300

    Desafío 3 - Correccion

--- desafio-3/index.js
+++ desafio-3/index.js
@@ -4,21 +4,23 @@ const getWords = (text, fin, sec = 1000) => {
     let i = 0;
     const interval = setInterval(()=>{
         if (i >= countWords) {
-            fin(interval, countWords) ;
+            clearInterval(interval);
+            console.log('Fin');
+            console.log('Total palabras: '+ countWords);
+            fin();
         } else {
             console.log(words[i]);
         }
         i++;  
     }, sec)
 }
-const fin = (interval, countWords) => {
-    clearInterval(interval);
-    console.log('Fin');
-    console.log('Total palabras: '+ countWords);
-}
 
-getWords('Kame hame ha', fin, 4000);
+const fin1 = () => getWords('Hoy hace mucho calor', fin2, 3000);
 
-getWords('Hoy hace mucho calor', fin, 3000);
 
-getWords('Hola', fin);
+const fin2 = () => getWords('Hola', ()=>{});
+
+
+
+getWords('Kame hame ha', fin1, 2000);
+
